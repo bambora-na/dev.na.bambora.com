f2f2e33d4b3074a0e1c15ba52a0e5633e1dc3f8d
Jesus Galvan <jesus.galvan@bambora.com>
2023-11-06 21:51:49 -0800

    BIC-5137
    Test

--- build_content.sh
+++ build_content.sh
@@ -25,37 +25,9 @@ then
 else
     ONBOARDING_HOST='onboardingapi'
 fi
-# BRANCH="${bamboo.shortPlanName}" APP_HOME=${bamboo.build.working.directory} 
-#BUILD_NUMBER=${bamboo.buildNumber} 
-#BUILD_TIME=${bamboo.buildTimeStamp} 
-#REVISION=${bamboo.planRepository.revision}
-
-# Set your variables
-#BRANCH_NAME=$BRANCH
-#BUILD_NUMBER=$BUILD_NUMBER
-#BUILD_TIME=$BUILD_TIME
-#REVISION=$REVISION
-
-#cat version.json | sed "s/\BRANCH/$BRANCH/g; s/\REVISION/$REVISION/g; s/\BUILD_TIME/$BUILD_TIME/g; s/\BUILD_NUMBER/$BUILD_NUMBER/g" > version.json
-
-
 
 echo "ONBOARDING_HOST is $ONBOARDING_HOST"
 mkdir -p build
-echo "APP_HOME PATH: $APP_HOME" 
-
-echo "sed BRANCH: $BRANCH"
-sed -i 's|BRANCH|$BRANCH|g' $APP_HOME/version.json
-
-echo "sed REVISION: $REVISION"
-sed -i 's|REVISION|$REVISION|g' $APP_HOME/version.json
-
-echo "sed BUILD_TIME: $BUILD_TIME"
-sed -i 's|BUILD_TIME|$BUILD_TIME|g' $APP_HOME/version.json
-
-echo "sed BUILD_NUMBER: $BUILD_NUMBER"
-sed -i 's|BUILD_NUMBER|$BUILD_NUMBER|g' $APP_HOME/version.json
-
 docker run -e ONBOARDING_HOST=${ONBOARDING_HOST} -v $APP_HOME/build:/usr/src/app/build dev.bambora.com static
 
 
