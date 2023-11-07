c455636821a9fe02ae57592c22b2a1d6f9bbad90
Jesus Galvan <jesus.galvan@bambora.com>
2023-11-07 14:44:57 -0800

    bic-5137
    xxxx

--- build_content.sh
+++ build_content.sh
@@ -35,6 +35,7 @@ sed -i 's|BUILD_TIME|'$BUILD_TIME'|g' $APP_HOME/version.json
 sed -i 's|BUILD_NUMBER|'$BUILD_NUMBER'|g' $APP_HOME/version.json
 
 cp $APP_HOME/version.json $APP_HOME/source/version.json
+cp $APP_HOME/version.json $APP_HOME/build/version.json
 
 #cat $APP_HOME/build/version.json
 #sed -i 's|BRANCH|'$BRANCH'|g' $APP_HOME/build/version.json
